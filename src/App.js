653d7fe99e326a055ae5890af3fd11cfeba2c34c
Aleksandr7739 <115591062+Aleksandr7739@users.noreply.github.com>
2023-04-09 22:45:05 +0300

    new files

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import { useEffect } from 'react';
-const tg = window.Telegram.WebApp;
+
+
 
 function App() {
 
@@ -8,15 +9,11 @@ function App() {
     tg.ready();
   }, [])
 
-  const onClose = () => {
-    tg.close()
-  }
-
-
   return (
     <div className="App">
-      <button onClick={onClose}>Закрыть</button>
+
     </div>
+
   );
 }
 
